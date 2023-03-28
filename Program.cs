35bf68a39b8b78b686a44c693a0fe161b1c8a98a
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-28 18:05:29 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HashTablesandBinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------HashTables-------------");
+            HashTables.PerformHashTables();
+            BinarySearchTree output = new BinarySearchTree();
+            output.Execute();
+        }
+    }
+}
